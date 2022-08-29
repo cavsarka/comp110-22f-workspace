f695a82323d75949283a50f8862e3561ac8680ed
Chinmay Avsarkar <cavsarka@unc.edu>
2022-08-29 15:57:27 -0400

    Final Chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -17,13 +17,30 @@ if len(input_character) != 1:
 print("Searching for " + input_character + " in " + input_word)
 
 match_counter = 0
-for num in range(0, 5):
-    if input_word[num] == input_character:
-        print(str(input_character) + " found at index " + str(num))
-        match_counter += 1
-    num += 1
+if input_word[0] == input_character:
+    print(input_character + " found at index 0")
+    match_counter += 1
+
+if input_word[1] == input_character:
+    print(input_character + " found at index 1")
+    match_counter += 1
+
+if input_word[2] == input_character:
+    print(input_character + " found at index 2")
+    match_counter += 1
+
+if input_word[3] == input_character:
+    print(input_character + " found at index 3")
+    match_counter += 1
+
+if input_word[4] == input_character:
+    print(input_character + " found at index 4")
+    match_counter += 1
+
 
 if match_counter == 0:
-    print("No intances of {} found in {}".format(input_character, input_word))
+    print("No instances of {} found in {}".format(input_character, input_word))
+elif match_counter == 1:
+    print("{} instance of {} found in {}".format(match_counter, input_character, input_word))
 else:
     print("{} instances of {} found in {}".format(match_counter, input_character, input_word))
